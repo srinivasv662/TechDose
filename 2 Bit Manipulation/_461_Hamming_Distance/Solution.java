c6be370f9b25dd4c2f5be44814c08ed1bc0ba3fd
Srinivas V <srinivasv662@gmail.com>
2022-06-10 23:26:39 +0530

    single elemet II

--- 2 Bit Manipulation/_461_Hamming_Distance/Solution.java
+++ 2 Bit Manipulation/_461_Hamming_Distance/Solution.java
@@ -29,3 +29,15 @@ class Solution {
 //         return cnt;
 //     }
 // }
+
+// class Solution {
+//     public int hammingDistance(int x, int y) {
+//         int z = x ^ y;
+//         int res = 0;
+//         while(z != 0){
+//             res += z & 1;
+//             z >>= 1;
+//         }
+//         return res;
+//     }
+// }
